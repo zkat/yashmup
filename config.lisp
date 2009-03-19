8e19cbb73ce0ac1fe6824e0aa4a9ab15384e3a5f
Josh <sykopomp@sykosomatic.org>
2009-03-19 01:19:13 -0400

    Adding more shit

---
+++ config.lisp
@@ -0,0 +1,11 @@
+(in-package :yashmup)
+
+(defparameter *screen-height* 800)
+(defparameter *screen-width* 500)
+(defparameter *bg-color* sdl:*black*)
+(defvar *running* nil)
+(defparameter *resource-path*
+  (merge-pathnames "resources/" 
+		   #+sbcl(values *default-pathname-defaults*)
+		   #+ccl(ccl::current-directory-name)
+		   #+clisp(ext:default-directory)))
