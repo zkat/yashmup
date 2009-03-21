d4bf7b91f7ddfd7c2e3767319e67458f95a23817
Kat Marchán <kzm@sykosomatic.org>
2009-03-20 23:23:01 -0400

    Added scrolling background.

---
+++ background.lisp
@@ -0,0 +1,18 @@
+(in-package :yashmup)
+
+(defclass background (sprite)
+  ((y :initform -1600)
+   (image :initform (gethash 'background *resource-table*))
+   (scroll-speed :initform 10 :accessor scroll-speed)))
+
+(defmethod update ((bg background))
+  (with-slots (y scroll-speed) bg
+    (if (<= 0 y)
+	(setf y -1600)
+	(incf y scroll-speed))))
+
+(defmethod draw ((bg background))
+  (with-slots (image x y)
+      bg
+    (sdl:draw-surface-at-* image x (+ y 1600)))
+  (call-next-method))
