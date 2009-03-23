5ee1fe30614496e04de368797640a14db8652287
Kat Marchán <kzm@sykosomatic.org>
2009-03-23 06:05:41 -0400

    time-change detection, new file, progress on main loop

--- src/main.lisp
+++ src/main.lisp
@@ -25,9 +25,8 @@
       (:idle ()
 	     ;; (execute-next-event (event-queue *game*)) <-- this is what I should be aiming at
 	     (if (and (framerate *game*) (> (framerate *game*) 0))
-		 (let* ((the-time (get-internal-real-time))
-			(seconds-since-last-frame (/ (- the-time (last-frame-time *game*)) 
-						     internal-time-units-per-second)))
+		 (multiple-value-bind (seconds-since-last-frame the-time)
+		     (time-difference (last-frame-time *game*))
 		   (when (> seconds-since-last-frame
 			    (/ 1 (framerate *game*)))
 		     (setf (last-frame-time *game*) the-time)
@@ -36,4 +35,3 @@
 		 (progn (take-a-step *game*) (sdl:update-display)))
 	     (when (not (running-p *game*))
 	       (sdl:push-quit-event))))))
-
---
+++ src/util/utils.lisp
@@ -0,0 +1,8 @@
+(in-package :yashmup)
+
+(defun time-difference (time-before)
+  "Checks the difference between the internal-time provided and the current time.
+Returns both the difference in time and the current-time used in the computation"
+  (let ((time-now (get-internal-real-time)))
+    (values (/ (- time-now time-before) internal-time-units-per-second)
+	    time-now)))
