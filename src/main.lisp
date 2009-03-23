290a1b92fc2bc855ec0dce7c954787e32b77cd2d
Kat Marchán <kzm@sykosomatic.org>
2009-03-23 17:15:33 -0400

    Sound works now

--- src/main.lisp
+++ src/main.lisp
@@ -7,37 +7,7 @@
 
 (defun main ()
   (sdl:with-init (sdl:sdl-init-video sdl:sdl-init-audio)
-    (sdl:initialise-default-font)
-    (sdl:window *screen-width* *screen-height*
-		:title-caption "PEW PEW"
-		:icon-caption "PEW PEW")
-    (setf (sdl:frame-rate) 0)
-    (sdl:clear-display *bg-color*)
-    (init-resources)
-    (setf *game* (make-instance 'game))
-    (sdl:with-events ()
-      (:quit-event () (prog1 t
-			(setf (running-p *game*) nil)))
-      (:key-down-event (:key key)
-		       (handle-key-event key *game*))
-      (:key-up-event (:key key)
-		     (handle-key-event key *game* :event-type :key-up))
-      (:idle ()
-	     (loop while (and (peek-next-event *game*)
-			      (cooked-p (peek-next-event *game*)))
-		do (execute-event (pop-next-event *game*)))
-	     (take-a-step *game*)
-	     (sdl:update-display)
-	     (multiple-value-bind (seconds-since-last-frame curr-time)
-		 (time-difference (last-frame-time *game*))
-	       (setf (last-frame-time *game*) curr-time)
-	       (sleep (let ((sleep-time (- (/ 1 (framerate *game*)) seconds-since-last-frame)))
-			(if (>= sleep-time 0)
-			    sleep-time
-			    0))))))))
-
-(defun sdl-controlled ()
-  (sdl:with-init (sdl:sdl-init-video sdl:sdl-init-audio)
+    (sdl-mixer:open-audio :chunksize 1024)
     (sdl:initialise-default-font)
     (sdl:window *screen-width* *screen-height*
 		:title-caption "PEW PEW"
@@ -46,19 +16,27 @@
     (sdl:clear-display *bg-color*)
     (init-resources)
     (setf *game* (make-instance 'game))
+    (sdl-mixer:play-music (find-resource 'music))
     (sdl:with-events ()
-      (:quit-event () (prog1 t
-			(setf (running-p *game*) nil)))
-      (:key-down-event (:key key)
-		       (handle-key-event key *game*))
-      (:key-up-event (:key key)
-		     (handle-key-event key *game* :event-type :key-up))
-      (:idle ()
-	     (loop while (and (peek-next-event *game*)
-			      (cooked-p (peek-next-event *game*)))
-		do (execute-event (pop-next-event *game*)))
-	     (take-a-step *game*)
-	     (sdl:update-display)))))
+       (:quit-event () (prog1 t
+			 (setf (running-p *game*) nil)
+			 (sdl-mixer:halt-music)
+			 (sdl-mixer:free (find-resource 'music))
+			 (let ((laser-sample (find-resource 'laser-sample)))
+			   (when laser-sample
+			     (sdl-mixer:free (gethash 'laser-sample *resource-table*))))
+			 (sdl-mixer:close-audio)))
+       (:key-down-event (:key key)
+			(handle-key-event key *game*))
+       (:key-up-event (:key key)
+		      (handle-key-event key *game* :event-type :key-up))
+       (:idle ()
+	      (loop while (and (peek-next-event *game*)
+			       (cooked-p (peek-next-event *game*)))
+		 do (execute-event (pop-next-event *game*)))
+	      (take-a-step *game*)
+	      (sdl:update-display)))))
+
 
 
 
--- src/resources.lisp
+++ src/resources.lisp
@@ -5,15 +5,25 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :yashmup)
 
-(defvar *resource-table* (make-hash-table :test #'eq))
+(defvar *resource-table* (make-hash-table))
 
 (defun load-image (name &key (extension ".bmp") color-key)
   (let ((image-path (merge-pathnames (concatenate 'string name extension) *resource-path*)))
     (sdl-image:load-image (namestring image-path) :alpha 255 :color-key color-key)))
 
-(defun load-sound (name)
+(defun load-sample (name)
   (let ((sound-path (merge-pathnames (concatenate 'string name ".wav") *resource-path*)))
     (sdl-mixer:load-sample (namestring sound-path))))
+
+(defun load-music (name &key (extension ".ogg"))
+  (let ((music-path (merge-pathnames (concatenate 'string name extension) *resource-path*)))
+    (sdl-mixer:load-music (namestring music-path))))
+
+(defun find-resource (name)
+  (if (symbolp name)
+      (let ((resource (gethash name *resource-table*)))
+	resource)
+      (error "resource name must be a symbol")))
 
 (defun init-resources ()
   (setf (gethash 'background *resource-table*)
@@ -35,5 +45,10 @@
   (setf (gethash 'enemy-large *resource-table*)
 	(load-image "enemy-large" :extension ".gif" :color-key (sdl:color :r 255 :g 255 :b 255)))
   (setf (gethash 'golden-boss *resource-table*)
-	(load-image "golden-boss" :extension ".gif" :color-key (sdl:color))))
+	(load-image "golden-boss" :extension ".gif" :color-key (sdl:color)))
+  (setf (gethash 'laser-sample *resource-table*)
+	(load-sample "pew"))
+  (setf (gethash 'music *resource-table*)
+	(load-music "game-music")))
+
 
