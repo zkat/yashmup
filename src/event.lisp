51b892b2b3ccc0215d8501c5c5d1daf4eb31c262
Kat Marchán <kzm@sykosomatic.org>
2009-03-23 06:30:20 -0400

    Improvements to event system

--- src/event.lisp
+++ src/event.lisp
@@ -18,10 +18,20 @@
     :documentation "A function, that contains the code to be executed.")
    (exec-time
     :initarg :exec-time
-    :initform (/ (* (get-internal-real-time) internal-time-units-per-second) 1000)
+    :initform (get-internal-real-time)
     :accessor exec-time
     :documentation "The time of execution when this event will be considered 'cooked'. 
 By default, all events are immediately cooked.")))
+
+(defun make-event (payload &key (delay 0))
+  "Creates a new event, with PAYLOAD being a function that contains everything to be executed.
+It also accepts a DELAY, in milliseconds, until the event is ready to go. Otherwise, it's
+ready immediately."
+  (let ((internal-delay-time (* delay (/ internal-time-units-per-second 1000))))
+    (push-event (make-instance 'event
+			       :payload payload 
+			       :exec-time (+ (get-internal-real-time) internal-delay-time))
+		*game*)))
 
 ;;;
 ;;; Event-queue
@@ -47,7 +57,7 @@ By default, all events are immediately cooked.")))
 
 (defmethod clear-events ((game game))
   (let ((pause-state (paused-p game)))
-   (setf (event-queue queue) (make-priority-queue :key #'exec-time))
+   (setf (event-queue game) (make-priority-queue :key #'exec-time))
    (setf (paused-p game) pause-state)))
 
 ;;;
@@ -77,9 +87,9 @@ By default, all events are immediately cooked.")))
 
 (defmethod cooked-p ((event event))
   "Simply checks that it doesn't shoot its load prematurely.."
-  (when (<= (exec-time event)
-	    (/ (* (get-internal-real-time) internal-time-units-per-second) 1000))
-    t))
+  (let  ((time-difference (time-difference (exec-time event))))
+    (when (>= time-difference 0)
+      t)))
 
 (defmethod execute-event ((event event))
   "Executes a standard event. Nothing fancy, just a funcall."
