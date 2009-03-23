e959f6bb07891b84ab0b5ee2ae1d4f4a5df30523
Kat Marchán <kzm@sykosomatic.org>
2009-03-23 07:47:04 -0400

    Stupid fucking sdl loop

--- src/main.lisp
+++ src/main.lisp
@@ -23,10 +23,42 @@
       (:key-up-event (:key key)
 		     (handle-key-event key *game* :event-type :key-up))
       (:idle ()
-;;	     (execute-next-event (event-queue *game*)) <-- this is what I should be aiming at
+	     (loop while (and (peek-next-event *game*)
+			      (cooked-p (peek-next-event *game*)))
+		do (execute-event (pop-next-event *game*)))
 	     (take-a-step *game*)
 	     (sdl:update-display)
-	     (sleep (/ 1 (framerate *game*)))))))
+	     (multiple-value-bind (seconds-since-last-frame curr-time)
+		 (time-difference (last-frame-time *game*))
+	       (setf (last-frame-time *game*) curr-time)
+	       (sleep (let ((sleep-time (- (/ 1 (framerate *game*)) seconds-since-last-frame)))
+			(if (>= sleep-time 0)
+			    sleep-time
+			    0))))))))
+
+(defun sdl-controlled ()
+  (sdl:with-init (sdl:sdl-init-video sdl:sdl-init-audio)
+    (sdl:initialise-default-font)
+    (sdl:window *screen-width* *screen-height*
+		:title-caption "PEW PEW"
+		:icon-caption "PEW PEW")
+    (setf (sdl:frame-rate) 60)
+    (sdl:clear-display *bg-color*)
+    (init-resources)
+    (setf *game* (make-instance 'game))
+    (sdl:with-events ()
+      (:quit-event () (prog1 t
+			(setf (running-p *game*) nil)))
+      (:key-down-event (:key key)
+		       (handle-key-event key *game*))
+      (:key-up-event (:key key)
+		     (handle-key-event key *game* :event-type :key-up))
+      (:idle ()
+	     (loop while (and (peek-next-event *game*)
+			      (cooked-p (peek-next-event *game*)))
+		do (execute-event (pop-next-event *game*)))
+	     (take-a-step *game*)
+	     (sdl:update-display)))))
 
 
 
