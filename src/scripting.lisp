5f7d5fbb60358e4546d674513c511938e9723bd2
Kat Marchán <kzm@sykosomatic.org>
2009-03-25 02:46:44 -0400

    Should probably keep these here

--- src/scripting.lisp
+++ src/scripting.lisp
@@ -11,3 +11,23 @@
 			      :payload (lambda () ,@body)
 			      :exec-frame (+ ,delay (current-frame *game*)))
 	       *game*))
+
+
+(defun move-in-curve (obj &optional (delta-theta 1))
+  (dotimes (i 600)
+    (fork (:delay (1+ i))
+      (incf (angle obj) delta-theta)))
+  (fork (:delay 600)
+    (detach obj *game*)))
+
+(defun move-in-rose (obj center-x center-y &optional (k 5))
+  (with-slots (x y angle velocity) obj
+   (let* ((initial-radius radius)
+	  (r (sqrt (+ (expt (- x center-x) 2)) (expt (- y center-y) 2)))
+	  (theta (atan y x)))
+     (dotimes (i 600)
+       (fork (:delay (* i))
+	 (incf theta)
+	 (setf radius (* initial-radius (cos (* k (degrees-to-radians theta)))))))))
+  (fork (:delay 600)
+    (detach obj *game*)))
