8afcf2d8898c68286526046ecded8949c6d77a96
Josh <sykopomp@sykosomatic.org>
2009-03-25 02:05:26 -0400

    Something is wrong, I can't find it

--- src/scripting.lisp
+++ src/scripting.lisp
@@ -5,11 +5,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :yashmup)
 
-(defmacro fork ((&key (delay 0) (repeat 1)) &body body)
+(defmacro fork ((&key (delay 0)) &body body)
   "Turns BODY into one or more event-loop events."
-  (let ((loop-var (gensym "loop-var")))
-    `(dotimes (,loop-var ,repeat)
-       (push-event (make-instance 'event 
-				  :payload (lambda () ,@body)
-				  :exec-frame (+ (* ,delay (1+ ,loop-var)) (current-frame *game*)))
-		   *game*))))
+  `(push-event (make-instance 'event 
+			      :payload (lambda () ,@body)
+			      :exec-frame (+ ,delay (current-frame *game*)))
+	       *game*))
