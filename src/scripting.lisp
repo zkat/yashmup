9df4c50e8fa588db106154ac98347e60d73e1609
Kat Marchán <kzm@sykosomatic.org>
2009-03-25 23:00:12 -0400

    New, improved fork macro

--- src/scripting.lisp
+++ src/scripting.lisp
@@ -5,46 +5,47 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :yashmup)
 
-(defmacro fork ((&key (delay 0)) &body body)
+(defmacro fork ((&key (delay 0) (repeat 0)) &body body)
   "Turns BODY into one or more event-loop events."
-  `(push-event (make-instance 'event 
-			      :payload (lambda () ,@body)
-			      :exec-frame (+ ,delay (current-frame *game*)))
-	       *game*))
+  (if (<= delay 0)
+      `(push-event (make-instance 'event 
+				  :payload (lambda () ,@body)
+				  :exec-frame (+ ,delay (current-frame *game*)))
+		   *game*)
+      `(labels ((make-one (repeat)
+		  (if (<= repeat 0)
+		      nil
+		      (progn
+			(push-event (make-instance 'event
+						   :payload (lambda ()
+							      ,@body)
+						   :exec-frame (+ ,delay (current-frame *game*)))
+				    *game*)
+			(push-event (make-instance 'event
+						   :payload (lambda ()
+							      (make-one (1- repeat)))
+						   :exec-frame (+ ,delay (current-frame *game*)))
+				    *game*)))))
+	 (make-one ,repeat))))
 
 (defun move-in-curve (obj &key (angle-delta 1) (num-frames 1))
-  (if (<= num-frames 0)
-      nil
-      (progn
-       (fork (:delay 1)
-	 (incf (angle obj) angle-delta))
-       (fork (:delay 1)
-	 (move-in-curve obj :angle-delta angle-delta :num-frames (1- num-frames))))))
+  (fork (:delay 1 :repeat num-frames)
+    (incf (angle obj) angle-delta)))
 
 (defun chase (obj target &key (num-frames 100))
-  (if (<= num-frames 0)
-      nil
-      (progn
-	(fork (:delay 1)
-	  (setf (angle obj) (angle-from obj target)))
-	(fork (:delay 1)
-	  (chase obj target :num-frames (1- num-frames))))))
+  (fork (:delay 1 :repeat num-frames)
+    (setf (angle obj) (angle-from obj target))))
 
 (defun move-in-orbit (obj target &key (keep-distance 100) (num-frames 100))
-  (if (<= num-frames 0)
-      nil
-      (progn
-	(fork (:delay 1)
-	  (let ((dist-difference (- (distance obj target)
-				    keep-distance)))
-	    (cond ((> dist-difference (+ 5 (velocity obj)))
-		   (setf (angle obj) (+ 1 (angle-from obj target))))
-		  ((< dist-difference 0)
-		   (setf (angle obj) (- (angle-from target obj) 1)))
-		  (t
-		   (setf (angle obj) (+ (angle-from obj target) 90))))))
-	(fork (:delay 1)
-	  (move-in-orbit obj target :keep-distance keep-distance :num-frames (1- num-frames))))))
+  (fork (:delay 1 :repeat num-frames)
+    (let ((dist-difference (- (distance obj target)
+			      keep-distance)))
+      (cond ((> dist-difference (+ 5 (velocity obj)))
+	     (setf (angle obj) (+ 1 (angle-from obj target))))
+	    ((< dist-difference 0)
+	     (setf (angle obj) (- (angle-from target obj) 1)))
+	    (t
+	     (setf (angle obj) (+ (angle-from obj target) 90)))))))
 
 ;; HA HA. NO
 ;; (defun move-in-rose (obj center-x center-y &optional (k 5))
