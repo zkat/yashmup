262e7f26b3e3f1d73fd57565b06bca294646e454
Kat Marchán <kzm@sykosomatic.org>
2009-03-30 19:40:35 -0400

    commenting test-level

--- levels/test-level.lisp
+++ levels/test-level.lisp
@@ -13,15 +13,21 @@
   (fork (:level level :delay 20 :repeat 3)
     (let ((enemy (make-instance 'enemy :x -15 :y 30 :velocity 4)))
       (attach enemy level)
+      ;; First start by moving to the right
       (setf (angle enemy) 90)
+      ;; 45 frames later, start moving in a clockwise curve
       (fork (:level level :delay 45)
 	(move-in-curve enemy :level level :num-frames 170 :angle-delta 3)
+	;; 50 frames after the curve-move is *started*...
 	(fork (:level level :delay 50)
+	  ;; Shoot 20 times, with a 5-frame delay between each shot
 	  (fork (:level level :delay 5 :repeat 20)
 	   (fire! enemy)))
+	;; Once we're done, we detach the enemy from the level
 	(fork (:level level :delay 500)
 	  (detach enemy level)))))
 
+  ;; right-hand side of first wave
   (fork (:level level :delay 20 :repeat 3)
     (let ((enemy (make-instance 'enemy :x 515 :y 30 :velocity 4)))
       (attach enemy level)
