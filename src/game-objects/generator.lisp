f8d2384408d6af5b2e365214677417d008f4de8c
Josh <sykopomp@sykosomatic.org>
2009-03-31 19:33:51 -0400

    Created new file for bullet generators.

---
+++ src/game-objects/generator.lisp
@@ -0,0 +1,8 @@
+;; This file is part of yashmup.
+
+;; generator.lisp
+;;
+;; Contains code related to bullet generators. including the generator class.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :yashmup)
