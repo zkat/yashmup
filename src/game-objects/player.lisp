847c94491236cf78bce8032e7da8eec09fad24da
Josh <sykopomp@sykosomatic.org>
2009-04-07 00:47:11 -0400

    sucks

--- src/game-objects/player.lisp
+++ src/game-objects/player.lisp
@@ -109,18 +109,7 @@
 
 (defmethod collided-p ((sprite-1 sprite) (sprite-2 player))
   "Checks whether two sprites have collided."
-  (let* ((sprite-1-hitbox-x (+ (x sprite-1) (hitbox-x-offset sprite-1)))
-	 (sprite-1-hitbox-y (+ (y sprite-1) (hitbox-y-offset sprite-1)))
-	 (sprite-2-hitbox-x (+ (x sprite-2) (hitbox-x-offset sprite-2)))
-	 (sprite-2-hitbox-y (+ (y sprite-2) (hitbox-x-offset sprite-2))))
-    ;; This version naively loops through all coordinates of both hitboxes, and finds overlaps.
-    (when (and (< sprite-1-hitbox-x sprite-2-hitbox-x)
-	       (> (+ (hitbox-width sprite-1) sprite-1-hitbox-x)
-		  sprite-2-hitbox-x)
-	       (< sprite-1-hitbox-y sprite-2-hitbox-y)
-	       (> (+ (hitbox-height sprite-1) sprite-1-hitbox-y)
-		  sprite-2-hitbox-y))
-      t)))
+)
 
 (defmethod collided-p ((sprite-1 player) (sprite-2 sprite))
   (collided-p sprite-2 sprite-1))
